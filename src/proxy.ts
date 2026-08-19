e07c2abeef834664afd457866830757afe369ef7
joaovtfor <joaovtfor@hotmail.com>
2026-08-19 15:55:39 -0300

    fix: rename export

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // Gera um Nonce (Number Used Once) forte para garantir que apenas scripts autorizados rodem
   const nonce = crypto.randomUUID();
 
